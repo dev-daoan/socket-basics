df08fc0ce78c52ea884e3bbf10cd7dc90613d7ec
Dao An <langtudatinh117@gmail.com>
2016-12-05 21:11:19 +0700

    Added custom message

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,9 @@ var socket = io();
 
 socket.on('connect', function() {
 	console.log('Connected to Socket.io server;');
-})
+});
+
+socket.on('message', function(message) {
+	console.log('New message');
+	console.log(message.text);
+});
